0326f887015c7f9460696b62be742bfa43910a3d
Vinicius Espindola <84110876+DEVinicius-jpeg@users.noreply.github.com>
2024-05-03 12:59:22 -0300

    avançando com o js

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const pokemonName = document.getElementById('pokemon-name');
 const pokemonId = document.getElementById('pokemon-id');
 const pokemonWeight = document.getElementById('weight');
 const pokemonHeight = document.getElementById('height');
-
+const spriteContainer = document.getElementById('sprite-container');
+const typesConteiner = document.getElementById('types-output');
 const searchForm = document.getElementById('search-form');
-const input = document.getElementById('search-input');
+
+const clearContents = ()=>{
+    pokemonName.classList.add('disable');
+    pokemonId.classList.add('disable');
+    pokemonWeight.classList.add('disable');
+    pokemonHeight.classList.add('disable');
+}
 
 const fetchPokemon = async ()=>{
+  clearContents();
+  const input = document.getElementById('search-input');
+  
   try{
-    const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${input}`);
+    const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${input.value}`);
     const data = await response.json(response.body);
+    // console.log(data.types);
+
     pokemonName.innerHTML = data.name.toUpperCase();
     pokemonId.innerHTML = `Nº ${data.id.toString().padStart(4, "0")}`;
-    pokemonWeight.innerHTML += data.height;
-    pokemonHeight.innerHTML += data.weight;
+    pokemonWeight.innerHTML = `Height: ${data.height}`;
+    pokemonHeight.innerHTML = `Weight: ${data.weight}`;
+    spriteContainer.innerHTML = `<img class="sprite-img" src="${data.sprites.front_default}" alt="${data.name}">`;
+    typesConteiner.innerHTML = data.types.map((type)=>{
+      return `<span id="type" class="${type.type.name}">${type.type.name}</span>`
+    }).join('');
+
+    pokemonName.classList.remove('disable');
+    pokemonId.classList.remove('disable');
+    pokemonWeight.classList.remove('disable');
+    pokemonHeight.classList.remove('disable');
 
   }catch(err){
-    console.log(err);
+    alert("Pokémon not found");
+    console.log(err.ht);
   }
 }
 
