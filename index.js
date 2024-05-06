9c7de854974bbc27bff0c6b91e7b325462134b65
Vinicius Espindola <84110876+DEVinicius-jpeg@users.noreply.github.com>
2024-05-06 14:31:36 -0300

    Perto do fim

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const pokemonHeight = document.getElementById('height');
 const spriteContainer = document.getElementById('sprite-container');
 const typesConteiner = document.getElementById('types-output');
 const searchForm = document.getElementById('search-form');
+const hpStats = document.getElementById('hp');
+const attackStats = document.getElementById('attack');
+const defenseStats = document.getElementById('defense');
+const spAttackStats = document.getElementById('special-attack');
+const spDefenseStats = document.getElementById('special-defense');
+const speedStats = document.getElementById('speed');
 
 const clearContents = ()=>{
     pokemonName.classList.add('disable');
     pokemonId.classList.add('disable');
     pokemonWeight.classList.add('disable');
     pokemonHeight.classList.add('disable');
+    spriteContainer.innerHTML = '';
+    typesConteiner.innerHTML = '';
+    hpStats.textContent = '';
+    attackStats.textContent = '';
+    defenseStats.textContent = '';
+    spAttackStats.textContent = '';
+    spDefenseStats.textContent = '';
+    speedStats.textContent = '';
 }
 
 const fetchPokemon = async ()=>{
@@ -19,8 +33,16 @@ const fetchPokemon = async ()=>{
   
   try{
     const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${input.value}`);
+    input.value = '';
     const data = await response.json(response.body);
-    // console.log(data.types);
+    console.log(data);
+
+    hpStats.textContent = data.stats[0].base_stat;
+    attackStats.textContent = data.stats[1].base_stat;
+    defenseStats.textContent = data.stats[2].base_stat;
+    spAttackStats.textContent = data.stats[3].base_stat;
+    spDefenseStats.textContent = data.stats[4].base_stat;
+    speedStats.textContent = data.stats[5].base_stat;
 
     pokemonName.innerHTML = data.name.toUpperCase();
     pokemonId.innerHTML = `Nº ${data.id.toString().padStart(4, "0")}`;
